eafb3f419e76fca82d2509e83c13a694760f777a
Victor <megazoid@protonmail.com>
2022-06-04 18:07:34 +0300

    Add swagger to project

--- src/main/java/telram/b7a/logproccessing/controller/LogProcessingController.java
+++ src/main/java/telram/b7a/logproccessing/controller/LogProcessingController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/logs")
+//@RequestMapping("/logs")
 @RequiredArgsConstructor
 public class LogProcessingController {
     private final LogParsingService logParsingService;
